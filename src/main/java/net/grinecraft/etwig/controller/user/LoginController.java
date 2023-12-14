536c50dbe9184931eeefd8a26fd272d4f6ea5663
Steven Webb <u7544998@anu.edu.au>
2023-12-14 21:51:30 +1100

    Better Freemarker

--- src/main/java/net/grinecraft/etwig/controller/user/LoginController.java
+++ src/main/java/net/grinecraft/etwig/controller/user/LoginController.java
@@ -1,17 +1,13 @@
 package net.grinecraft.etwig.controller.user;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import freemarker.template.Configuration;
-import freemarker.template.Template;
-
 import net.grinecraft.etwig.util.type.NavBar;
 
 @RestController
@@ -21,19 +17,22 @@ public class LoginController {
     Configuration configuration;
 	
     @GetMapping("/user/login")
-    public String login() throws Exception {
+    public String login(Model model) throws Exception {
     	
     	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = (authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getPrincipal()))
-                            ? authentication.getName()
-                            : "guest";
+        //String username = (authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getPrincipal()))
+        //                    ? authentication.getName()
+        //                    : "guest";
         
-        System.out.println(username);
+        //System.out.println(username);
         
-        HashMap<String, Object> templateMap = new HashMap<String, Object>();
-		templateMap.put("navbar", NavBar.LOGIN);
+        //HashMap<String, Object> templateMap = new HashMap<String, Object>();
+		//templateMap.put("navbar", NavBar.LOGIN);
 		
-    	Template template = configuration.getTemplate("user/login.ftl");
-        return FreeMarkerTemplateUtils.processTemplateIntoString(template, templateMap);
+    	//Template template = configuration.getTemplate("user/login.ftl");
+        //return FreeMarkerTemplateUtils.processTemplateIntoString(template, templateMap);
+        
+        model.addAttribute("navbar", NavBar.LOGIN);
+		return "user/login";
     }
 }
