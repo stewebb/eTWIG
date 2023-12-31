7e3b5d1e8c511e7a91d91a2134278b707b691ba1
Steven Webb <u7544998@anu.edu.au>
2023-12-31 15:10:30 +1100

    Get TWIG template - Frontend

--- src/main/resources/static/static/js/etwig-twig-draw.js
+++ src/main/resources/static/static/js/etwig-twig-draw.js
@@ -2,13 +2,26 @@ MIN_WINDOW_WIDTH = 1280;
 MIN_WINDOW_HEIGHT = 720;
 ASPECT_RATIO = [16, 9];
 
+var twigHasTemplate = false;
+
+var backgroundImageURL = "";
 var backgroundImage;
 
 function preload(){
-    backgroundImage = loadImage("https://raw.githubusercontent.com/Rabbid76/graphics-snippets/master/resource/texture/background.jpg")
+	getTWIGTemplate();
+	if(!twigHasTemplate){
+		return;
+	}
+	
+
+    backgroundImage = loadImage(backgroundImageURL);
+	//backgroundImage = 255;
 }
 
 function setup() {
+	if(!twigHasTemplate){
+		return;
+	}
 	//var TWIGResulution = setTWIGResolution();
 	//createCanvas(TWIGResulution[0], TWIGResulution[1]);
 }
@@ -25,12 +38,14 @@ function draw() {
  	
  	//stroke(255, 0, 0);
  	//rect(0, 0, TWIGResulution[0], TWIGResulution[1]);
- 	
+ 	if(!twigHasTemplate){
+		return;
+	}
  	background(backgroundImage);
 }
 
 /**
- * Set the TWIG resolution based on the current web window resolution, with the following rules:
+ * Set the TWIG canvas size based on the current web window resolution, with the following rules:
  * 1. The minimun resolution applies.
  * 2. A fixed aspect ratio applies.
  * @returns A 2-element array that contains TWIG width and TWIG height.
@@ -41,4 +56,41 @@ function setTWIGResolution(){
 	var TWIGWindowHeight =  max(MIN_WINDOW_WIDTH, windowHeight);
 	TWIGWindowHeight = TWIGWindowWidth * ASPECT_RATIO [1] / ASPECT_RATIO[0];
 	return [TWIGWindowWidth, TWIGWindowHeight];
+}
+
+function getTWIGTemplate(){
+	var url = '/api/public/getTwigTemplateById';
+	
+	$.ajax({ 
+		type: 'GET', 
+    	url: url, 
+    	async: false,
+    	data: { 
+			templateId: 3,
+		}, 
+    	dataType: 'json',
+		success: getTWIGTemplateWhenSuccess,
+    	error: function(err) {   		
+			alert("Failed to get TWIG content due to a HTTP " + err.status + " error.\n" + err.responseJSON.exception);
+		}
+	});
+}
+
+function getTWIGTemplateWhenSuccess(json){
+	
+	// No HTTP error, but the input is invalid or other error happens.
+	if(json.error > 0){
+		alert("Failed to get TWIG template.\n"+ json.msg);
+		return;
+	}
+	
+	// Template cannot be found.
+	if(json.template == null){
+		alert("Cannot find a TWIG template by the given condition.");
+		return;
+	}
+	
+	// Step 1: Set the background Image URL.
+	backgroundImageURL = "/twig/assets?assetId=" + json.template.backgroundImage;
+  	twigHasTemplate = true;
 }
