444bfe0b169897e29edd59abf919b9c36e433d26
Steven Webb <u7544998@anu.edu.au>
2024-01-28 20:02:32 +1100

    b

--- src/main/resources/static/static/js/etwig/twig.js
+++ src/main/resources/static/static/js/etwig/twig.js
@@ -24,25 +24,13 @@ class TwigNode{
         // Only the node of template widget is allowed to add child.
         if(this.widget instanceof Template){
             this.children.push(childNode);
-        }
-
-        // Display a warning
-        else{
+        }else{
             console.warn("Only the node of template widget is allowed to add child.");
         }
         
     }
 
     setWidget(widget){
-
-        // Template widget, internal node only.
-        //if (widget instanceof Template && this.isLeafNode()){
-        //    throw new Error("A template widget cannot be added to a leaf node!");
-        //}
-
-        //if (!(widget instanceof Template) && !this.isLeafNode()){
-        //    throw new Error("A non-template widget cannot be added to an internal node!")
-        //}
         this.widget = widget;
     }
 
@@ -69,6 +57,10 @@ class TwigNode{
 
 class Template {
 
+    constructor(){
+        this.type = "TEMPLATE";
+    }
+
     /**
      * **The Template object** is a group of widgets with a rectangle area. 
      * **The widget of the root node should be a template.** 
@@ -79,7 +71,7 @@ class Template {
      * @param {int} height The height of the template area
      */
 
-    constructor(posX, posY, width, height) {
+    setValues(posX, posY, width, height) {
         this.posX = posX;
         this.posY = posY;
         this.width = width;
@@ -89,11 +81,19 @@ class Template {
     toString(){
         return "Template(" + this.posX + ", " + this.posY + ", " + this.width + ", " + this.height + ")"; 
     }
+
+    //toJson(){
+    //    return {type: "TEMPLATE", posX: this.posX, posY: this.posY, width:this.width, height: this.height};
+    //}
 }
 
 class Image {
 
-    constructor(assetId, posX, posY, width) {
+    constructor(){
+        this.type = "IMAGE";
+    }
+
+    setValues(assetId, posX, posY, width) {
         this.assetId = assetId;
         this.posX = posX;
         this.posY = posY;
@@ -104,10 +104,18 @@ class Image {
         return "Image(" + this.assetId + ", " + this.posX + ", " + this.posY + ", " + this.width + ")"; 
     }
 
+    //toJson(){
+    //    return {type: "IMAGE"};
+    //}
 }
 
 class EventTables {
-    constructor(posX, posY, width, height, dayStart, dayEnd) {
+
+    constructor(){
+        this.type = "EVENT_TABLES";
+    }
+
+    setValues(posX, posY, width, height, dayStart, dayEnd) {
         this.posX = posX;
         this.posY = posY;
         this.width = width;
@@ -119,25 +127,122 @@ class EventTables {
     toString(){
         return `EventTables(${this.posX}, ${this.posY}, ${this.width}, ${this.height}, ${this.dayStart}, ${this.dayEnd})`; 
     }
+
+    //toJson(){
+    //    return {type: "EVENT_TABLES"};
+    //}
 }
+
 
 // A 1920x1080 canvas
 var root = new TwigNode();
-root.setWidget(new Template(0, 0, 1920, 1080));
+
+var template = new Template();
+template.setValues(0, 0, 1920, 1080);
+root.setWidget(template);
 
 // A 1920x1080 background
 var background = new TwigNode();
-background.setWidget(new Image(1, 0, 0, 1920));
+
+var b = new Image();
+b.setValues(1, 0, 0, 1920);
+background.setWidget(b);
 
 // The title area
 var titleArea = new TwigNode();
-titleArea.setWidget(new Template(0, 0, 1920, 1080));
+
+var title = new Template()
+title.setValues(0, 0, 1920, 1080);
+titleArea.setWidget(title);
 
 var logo = new TwigNode();
-logo.setWidget(new Image(2, 0, 0, 240));
+var w = new Image();
+w.setValues(2, 0, 0, 240);
+logo.setWidget(w);
 titleArea.addChild(logo)
 
 root.addChild(background);
 root.addChild(titleArea);
 
 root.printTree();
+
+
+function serializeTwigNode(node) {
+    if (!node) return null;
+
+    let jsonObject = {};
+
+    // Assuming the widget has a method to convert itself to JSON
+    // If not, you'll need to implement this part based on your Widget class structure
+    if (node.widget) {
+        jsonObject.widget = node.widget;
+    }
+
+    if (node.children.length > 0) {
+        jsonObject.children = node.children.map(child => serializeTwigNode(child));
+    }
+
+    return jsonObject;
+}
+
+function createWidgetFromJson(widgetJson) {
+    console.log(widgetJson);
+    switch (widgetJson.type) {
+        case 'IMAGE':
+            return new Image(widgetJson);
+        case 'EVENT_TABLES':
+            return new EventTables(widgetJson);
+        case 'TEMPLATE':
+            return new Template(widgetJson);
+        default:
+            throw new Error('Unknown widget type');
+    }
+}
+
+function deserializeTwigNode(jsonObject) {
+    if (!jsonObject) return null;
+
+    let widget = jsonObject.widget ? createWidgetFromJson(jsonObject.widget) : null;
+    let node = new TwigNode(widget);
+
+    if (jsonObject.children && jsonObject.children.length > 0) {
+        jsonObject.children.forEach(childJson => {
+            node.children.push(deserializeTwigNode(childJson));
+        });
+    }
+
+    return node;
+}
+
+let jsonTree = serializeTwigNode(root);
+jsonTree = JSON.stringify(jsonTree, null, 4);
+console.log(jsonTree);
+
+//let jsonObject = JSON.parse(jsonTree);
+
+//let rootNode = deserializeTwigNode(jsonObject);
+//console.log(rootNode);
+
+/*
+var json = JSON.stringify(root);
+
+function deserializeTree(serializedNode) {
+    let node = new TwigNode();
+    node.setWidget(serializedNode.widget);
+
+    if (serializedNode.children) {
+        serializedNode.children.forEach(child => {
+            node.addChild(deserializeTree(child));
+        });
+    }
+
+    return node;
+}
+
+let parsedObject = JSON.parse(json);
+
+let rootNode = deserializeTree(parsedObject);
+
+rootNode.printTree();
+
+*/
