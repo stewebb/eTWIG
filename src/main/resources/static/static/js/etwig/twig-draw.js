d7cf9e2389203d47c019eee643c3e02413c9ce65
root <root@FREEBSD>
2024-02-01 13:31:28 +1100

    draw

--- src/main/resources/static/static/js/etwig/twig-draw.js
+++ src/main/resources/static/static/js/etwig/twig-draw.js
@@ -7,54 +7,71 @@ function preload() {
     twig = new TWIG();
     twig.createTree(setting);
 
-    //twig.root.printTree();
-    //return;
-    //console.log(JSON.stringify(twig.serialize(), null, 2))
-
-    // Step 1: Get the background.
+    // Create the canvas first !!!!!
     background = twig.root.widget;
-    //console.log(background)
     createCanvas(background.width, background.height);
 
     //return;
 
+  
+
+    // Iterate all nodes via DFS, but only get the assets.
+    const iterator = new TwigNodeIterator(twig.root);
+    assetCollection = new Map();
+
+    while(iterator.hasNext()){
+        const { value, done } = iterator.next();
+        var widget = value.node.widget;
+        switch (widget.type){
+            case "IMAGE":
+                assetCollection.set(widget.assetId, loadImage("/assets/getPublicAsset?assetId=" + widget.assetId));
+                break;
+        }
+    }
+    console.log(assetCollection)
+}
+
+function setup(){
+    frameRate(10);
+}
+
+function draw() {
+
     // Track the depth of last iteration!
     var lastDepth = 0;
 
+    // Iterate the tree again to draw it. 
     const iterator = new TwigNodeIterator(twig.root);
-    
-    assetCollection = new Map();
-
-    //while (iterator.hasNext()) {
-    //    const { value, done } = iterator.next();
+    while (iterator.hasNext()) {
+        const { value, done } = iterator.next();
 
         // The current depth
-        //const { node, depth } = value;
+        const { node, depth } = value;
 
         /**
          * Get the parent node
         */
 
         // The path from root to current node.
-    //    var path = twig.findPath(value.node);
+        var path = twig.findPath(value.node);
 
         // The path contains at least 2 nodes, the penultimate node is parent, or the parent is itself.
-    //    var parent = (path.length > 1) ? path[path.length-2] : path[0];
+        var parent = (path.length > 1) ? path[path.length-2] : path[0];
 
         // Depth change value, 1 means deeper, 0 means same depth, -1 means shallower.
-    //    var depthChange = depth - lastDepth;
+        var depthChange = depth - lastDepth;
 
-    //    if(depthChange > 0){
+        if(depthChange > 0){
             //console.log(parent.widget.posX, parent.widget.posY)
-    //        translate(parent.widget.posX, parent.widget.posY);
-    //        push();
-   //     }else if(depthChange < 0){
-   //         pop();
-    //        push();
-    //    }
+            translate(parent.widget.posX, parent.widget.posY);
+            push();
+        }else if(depthChange < 0){
+            pop();
+            push();
+        }
 
         // Current widget
-    //    var widget = value.node.widget;
+        var widget = value.node.widget;
 
        // console.log(depth - lastDepth, widget.type, parent.widget)
 
@@ -76,45 +93,6 @@ function preload() {
    //     }
 
    //     lastDepth = depth;
-   // }
+   }
 
-   // console.log(assetCollection)
-
-    /*
-    while (iterator.hasNext()) {
-        const { value, done } = iterator.next();
-        const { node, depth } = value;
-
-        console.log(value)
-        
-        
-
-        switch (widget.type){
-            case "TEMPLATE":
-
-                console.log(widget)
-                fill(random(255), random(255), random(255));
-
-                //console.log(widget.posX, widget.posY, widget.width, widget.height);
-
-                //rect(0,0,100,100)
-                rect(widget.posX, widget.posY, widget.width, widget.height);
-                break;
-        }
-            
-
-    }
-
-    */
-}
-
-function setup(){
-
-}
-
-function draw() {
-
- 
-
-    //background(220);
 }
