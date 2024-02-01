93302c41233496b1f4d3b9d19e29fe374fc91587
root <root@FREEBSD>
2024-02-01 13:02:53 +1100

    draw

--- src/main/resources/static/static/js/etwig/twig-draw.js
+++ src/main/resources/static/static/js/etwig/twig-draw.js
@@ -1,4 +1,4 @@
-function setup() {
+function preload() {
 
     var setting = new TwigSettings();
     //setting.setPortfolio(1);
@@ -7,71 +7,78 @@ function setup() {
     twig = new TWIG();
     twig.createTree(setting);
 
+    //twig.root.printTree();
+    //return;
+    //console.log(JSON.stringify(twig.serialize(), null, 2))
 
-    
-    twig.root.printTree();
-    return;
-    console.log(JSON.stringify(twig.serialize(), null, 2))
-
+    // Step 1: Get the background.
     background = twig.root.widget;
     //console.log(background)
-
     createCanvas(background.width, background.height);
+
+    //return;
 
     // Track the depth of last iteration!
     var lastDepth = 0;
 
     const iterator = new TwigNodeIterator(twig.root);
     
-    while (iterator.hasNext()) {
-        const { value, done } = iterator.next();
+    assetCollection = new Map();
+
+    //while (iterator.hasNext()) {
+    //    const { value, done } = iterator.next();
 
         // The current depth
-        const { node, depth } = value;
+        //const { node, depth } = value;
 
         /**
          * Get the parent node
         */
 
         // The path from root to current node.
-        var path = twig.findPath(value.node);
+    //    var path = twig.findPath(value.node);
 
         // The path contains at least 2 nodes, the penultimate node is parent, or the parent is itself.
-        var parent = (path.length > 1) ? path[path.length-2] : path[0];
+    //    var parent = (path.length > 1) ? path[path.length-2] : path[0];
 
         // Depth change value, 1 means deeper, 0 means same depth, -1 means shallower.
-        var depthChange = depth - lastDepth;
+    //    var depthChange = depth - lastDepth;
 
-        if(depthChange > 0){
-            console.log(parent.widget.posX, parent.widget.posY)
-            translate(parent.widget.posX, parent.widget.posY);
-            push();
-        }else if(depthChange < 0){
-            pop();
-            push();
-        }
+    //    if(depthChange > 0){
+            //console.log(parent.widget.posX, parent.widget.posY)
+    //        translate(parent.widget.posX, parent.widget.posY);
+    //        push();
+   //     }else if(depthChange < 0){
+   //         pop();
+    //        push();
+    //    }
 
         // Current widget
-        var widget = value.node.widget;
+    //    var widget = value.node.widget;
 
        // console.log(depth - lastDepth, widget.type, parent.widget)
 
-        switch (widget.type){
-            case "TEMPLATE":
-                noFill();   strokeWeight(5);    stroke(0);
-                rect(widget.posX, widget.posY, widget.width, widget.height);
-                break;
+   //    console.log(0)
+   //     switch (widget.type){
+   //         case "TEMPLATE":
+   //             //noFill();   strokeWeight(5);    stroke(0);
+    //            //rect(widget.posX, widget.posY, widget.width, widget.height);
+    //            break;
 
-            case "IMAGE":
-            case "EVENT_TABLES":
-                fill(random(255), random(255), random(255));    noStroke();
-                rect(widget.posX, widget.posY, widget.width, widget.width);
-                console.log(widget.posX, widget.posY, widget.width, widget.width);
-                break;
-        }
+      //      case "IMAGE":
+            //case "EVENT_TABLES":
+                //fill(random(255), random(255), random(255));    noStroke();
+                //rect(widget.posX, widget.posY, widget.width, widget.width);
+                //console.log(widget.posX, widget.posY, widget.width, widget.width);
+           //     assetCollection.set(widget.assetId, loadImage("/assets/getPublicAsset?assetId=" + widget.assetId));
+                //p=( loadImage("/assets/getPublicAsset?assetId=" + widget.assetId))
+            //    break;
+   //     }
 
-        lastDepth = depth;
-    }
+   //     lastDepth = depth;
+   // }
+
+   // console.log(assetCollection)
 
     /*
     while (iterator.hasNext()) {
@@ -99,6 +106,10 @@ function setup() {
     }
 
     */
+}
+
+function setup(){
+
 }
 
 function draw() {
