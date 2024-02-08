69667750a809f1707cd4b3afd0d3352df0ae234f
root <root@FREEBSD.modem>
2024-02-08 18:28:51 +1100

    g

--- src/main/java/net/grinecraft/etwig/controller/api/TwigAPIController.java
+++ src/main/java/net/grinecraft/etwig/controller/api/TwigAPIController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.grinecraft.etwig.dto.TwigTemplateBasicInfoDTO;
 import net.grinecraft.etwig.dto.TwigTemplateDTO;
-import net.grinecraft.etwig.services.AssetService;
 import net.grinecraft.etwig.services.EventGraphicsService;
 import net.grinecraft.etwig.services.TwigService;
 import net.grinecraft.etwig.services.WeekService;
@@ -92,7 +91,7 @@ public class TwigAPIController {
         return twigService.getTwigTemplateList(page, size);
     }
 	
-	@RequestMapping("/api/private/getEventsByDate")  
+	@RequestMapping("/api/public/getEventsByDate")  
 	public void getEventsByDate(@RequestParam String date) {
 		eventGraphicsService.getGraphicsByDate(DateUtils.safeParseDate(date, "yyyy-MM-dd"));
 	}
--- src/main/java/net/grinecraft/etwig/repository/EventGraphicsRepository.java
+++ src/main/java/net/grinecraft/etwig/repository/EventGraphicsRepository.java
@@ -3,17 +3,21 @@ package net.grinecraft.etwig.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import net.grinecraft.etwig.model.EventGraphics;
 
 @Repository
-public interface EventGraphicsRepository {
+public interface EventGraphicsRepository extends JpaRepository<EventGraphics, Long>{
 
-	@Query("SELECT g FROM EventGraphics g "
-			+ "WHERE e.startTime > :start AND e.startTime < end"
-			+ "LEFT JOIN Event e"
-			+ "ON g.eventId = e.id")
-	public List<EventGraphics> getGraphicsListByDateRange(LocalDate start, LocalDate end);
+	//@Query("SELECT g FROM EventGraphics g "
+	//	       + "LEFT JOIN Event e ON g.eventId = e.id "
+	//	       + "WHERE e.startTime > :start AND e.startTime < :end")
+	//@Query("SELECT * FROM etwig_event_graphics g JOIN etwig_event e ON g.event = e.id WHERE e.start_time > :dts AND e.start_time < :dte", nativeQuery = true)
+
+	@Query(value = "SELECT * FROM etwig_event_graphics g JOIN etwig_event e ON g.event = e.id WHERE e.start_time > :dts AND e.start_time < :dte", nativeQuery = true)
+	public List<EventGraphics> getGraphicsListByDateRange(@Param("dts") LocalDate start,  @Param("dte")LocalDate end);
 }
