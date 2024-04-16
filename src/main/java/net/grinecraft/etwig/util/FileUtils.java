85a3e1960b04f9f72d2adf9375d81304c5841d6d
Steven Webb <u7544998@anu.edu.au>
2024-04-16 19:12:16 +1000

    change package name

--- src/main/java/net/grinecraft/etwig/util/FileUtils.java
+++
@@ -1,5 +0,0 @@
-package net.grinecraft.etwig.util;
-
-public class FileUtils extends org.apache.commons.io.FileUtils{
-
-}
--- src/main/java/net/grinecraft/etwig/util/exception/InvalidDateFormatException.java
+++
@@ -1,9 +0,0 @@
-package net.grinecraft.etwig.util.exception;
-
-public class InvalidDateFormatException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-	public InvalidDateFormatException(String message) {
-        super(message);
-    }
-}
