090509d8b419ce9779da4c4fb10a46d95de4c2a6
Steven Webb <u7544998@anu.edu.au>
2024-07-03 22:18:04 +1000

    updated version

--- src/main/java/net/etwig/webapp/EtwigInterceptor.java
+++ src/main/java/net/etwig/webapp/EtwigInterceptor.java
@@ -60,7 +60,7 @@ public class EtwigInterceptor implements HandlerInterceptor{
 		LinkedHashMap<String, Object> appInfo = new LinkedHashMap<>();
 		appInfo.put("appName", config.getAppName());
 
-		appInfo.put("appVersion", "3.4");
+		appInfo.put("appVersion", "3.5");
 		appInfo.put("appOwner", config.getAppOwner());
 		modelAndView.addObject("app", appInfo);
 	}
